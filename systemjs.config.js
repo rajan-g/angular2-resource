babe7e08bf53e8ce4a0587e577a67e77d2d03bde
Rajan G <rajan.ameex@gmail.com>
2016-07-11 20:40:02 +0530

    upgraded to rd4

--- systemjs.config.js
+++ systemjs.config.js
@@ -24,12 +24,10 @@
     'common',
     'compiler',
     'core',
-    'forms',
     'http',
     'platform-browser',
     'platform-browser-dynamic',
     'router',
-    'router-deprecated',
     'upgrade',
   ];
   // Individual files (~300 requests):
